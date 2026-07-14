bd64c4c3745303d17be5694e662f028416b9d96b
Ansley Smith <ansleysmith3@outlook.com>
2026-07-14 10:46:56 -0400

    testing edits from class

--- orders_plot.py
+++ orders_plot.py
@@ -2,6 +2,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+#Object Oriented Programming (OOP) - nouns have verbs
+    #df.head()
+#Functional programming - verbs applied to nounsn(ex: R)
+    #head(df)
+    
 orders = pd.read_csv("data/orders.csv")
 
 print("First rows")
